114ff3aba81992dbb93020a2abba4bf52a045c87
Frank Buckley <frank@dsegroup.org>
2023-06-23 18:07:50 +0100

    Utf8String: write bytes directly for Json

---
+++ src/DSE.Open/Text/Json/Serialization/JsonExceptionHelper.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Text.Json;
+
+namespace DSE.Open.Text.Json.Serialization;
+
+public static class JsonExceptionHelper
+{
+    public static void ThrowJsonException(string? message)
+        => throw new JsonException(message);
+
+    public static void ThrowJsonException(string? message, Exception? innerException)
+        => throw new JsonException(message, innerException);
+
+    public static void ThrowJsonException(
+        string? message,
+        string? path,
+        long? lineNumber,
+        long? bytePositionInLine,
+        Exception? innerException)
+        => throw new JsonException(message, path, lineNumber, bytePositionInLine, innerException);
+}
--- src/DSE.Open/Text/Json/Serialization/JsonStringUtf8StringConverter.cs
+++ src/DSE.Open/Text/Json/Serialization/JsonStringUtf8StringConverter.cs
@@ -1,11 +1,31 @@
-﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DSE.Open.Text.Json.Serialization;
 
-public sealed class JsonStringUtf8StringConverter : SpanParsableCharWritingJsonConverter<Utf8String>
+public sealed class JsonStringUtf8StringConverter : JsonConverter<Utf8String>
 {
     public static readonly JsonStringUtf8StringConverter Default = new();
 
-    protected override int GetMaxCharCountToWrite(Utf8String value) => value.Length;
+    public override Utf8String Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+        {
+            JsonExceptionHelper.ThrowJsonException("Expected string value");
+        }
+
+        return new(reader.ValueSpan.ToArray());
+    }
+
+    public override void Write(Utf8JsonWriter writer, Utf8String value, JsonSerializerOptions options)
+    {
+        Guard.IsNotNull(writer);
+        writer.WriteStringValue(value.Span);
+    }
 }
