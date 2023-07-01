fba9f3096e99eda5c35d1e72df183e1a70bece97
Frank Buckley <frank@frankbuckley.com>
2023-07-01 13:10:22 +0100

    Add AsciiPathRouteConstraint

---
+++ src/DSE.Open.Web/Routing/AsciiPathRouteConstraint.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Globalization;
+using DSE.Open.Values;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Matching;
+
+namespace DSE.Open.Web.Routing;
+
+public class AsciiPathRouteConstraint : IRouteConstraint, IParameterLiteralNodeMatchingPolicy
+{
+    /// <inheritdoc />
+    public bool Match(
+        HttpContext? httpContext,
+        IRouter? route,
+        string routeKey,
+        RouteValueDictionary values,
+        RouteDirection routeDirection)
+    {
+        Guard.IsNotNull(routeKey);
+        Guard.IsNotNull(values);
+
+        if (values.TryGetValue(routeKey, out var value) && value is not null)
+        {
+            if (value is AsciiPath)
+            {
+                return true;
+            }
+
+            var valueString = Convert.ToString(value, CultureInfo.InvariantCulture);
+            return CheckConstraintCore(valueString);
+        }
+
+        return false;
+    }
+
+    private static bool CheckConstraintCore(string? valueString)
+        => string.IsNullOrEmpty(valueString) || AsciiPath.IsValidValue(valueString, true);
+
+    public bool MatchesLiteral(string parameterName, string literal)
+        => CheckConstraintCore(literal);
+}
