2bb257f19067542b533af7d618690e71094f46f8
Frank Buckley <frank@dsegroup.org>
2023-07-04 15:39:19 +0100

    Add LikePatternToStringConverter

---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/LikePatternToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using DSE.Open.Values.Text;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class LikePatternToStringConverter : ValueConverter<LikePattern, string>
+{
+    public static readonly LikePatternToStringConverter Default = new();
+
+    public LikePatternToStringConverter() : base(v => ConvertTo(v), v => ConvertFrom(v))
+    {
+    }
+
+    private static string ConvertTo(LikePattern value) => value.ToString();
+
+    private static LikePattern ConvertFrom(string value) => new(value);
+}
