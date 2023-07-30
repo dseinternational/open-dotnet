0ba3905ca3e599e9c02cc7efd506215cbcc92f10
Frank Buckley <frank@dsegroup.org>
2023-07-30 08:53:02 +0100

    Add DateTimeOffsetToUtcTicksConverter

---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/DateTimeOffsetToUtcTicksConverter.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class DateTimeOffsetToUtcTicksConverter : ValueConverter<DateTimeOffset, long>
+{
+    public static readonly DateTimeOffsetToUtcTicksConverter Default = new();
+
+    public DateTimeOffsetToUtcTicksConverter()
+        : base(v => ToUtcTicks(v), v => FromUtcTicks(v))
+    {
+    }
+
+    private static long ToUtcTicks(DateTimeOffset value) => value.UtcTicks;
+
+    private static DateTimeOffset FromUtcTicks(long value) => new(value, TimeSpan.Zero);
+}
