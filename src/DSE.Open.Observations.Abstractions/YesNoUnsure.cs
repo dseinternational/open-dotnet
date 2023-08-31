47ea26a73ce90e8983384a14689317d063e44e53
Frank Buckley <frank@frankbuckley.com>
2023-08-31 14:51:39 +0100

    Start DSE.Open.Observations

---
+++ src/DSE.Open.Observations.Abstractions/YesNoUnsure.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+using DSE.Open.Values;
+using DSE.Open.Values.Text.Json.Serialization;
+
+namespace DSE.Open.Observations.Abstractions;
+
+/// <summary>
+/// Represents the selection of a choice between "Yes", "No" and "Not sure"/"Unsure".
+/// To represent no response, use nullable.
+/// </summary>
+[EquatableValue]
+[JsonConverter(typeof(JsonSpanSerializableValueConverter<YesNoUnsure, AsciiString>))]
+[StructLayout(LayoutKind.Auto)]
+public readonly partial struct YesNoUnsure : IEquatableValue<YesNoUnsure, AsciiString>
+{
+    public static int MaxSerializedCharLength => 6;
+
+    public static bool IsValidValue(AsciiString value)
+    {
+        return value.Length > 1 && value.Length <= 6 && Lookup.ContainsKey(value);
+    }
+
+    public static readonly YesNoUnsure Yes = new((AsciiString)"yes", true);
+
+    public static readonly YesNoUnsure No = new((AsciiString)"no", true);
+
+    public static readonly YesNoUnsure Unsure = new((AsciiString)"unsure", true);
+
+
+    public static readonly IReadOnlyCollection<YesNoUnsure> All = new[]
+    {
+        Yes,
+        No,
+        Unsure
+    };
+
+    public static readonly IReadOnlyDictionary<AsciiString, YesNoUnsure> Lookup = All.ToDictionary(r => r._value);
+}
---
+++ test/DSE.Open.Observations.Abstractions.Tests/YesNoUnsureTests.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Text.Json;
+using DSE.Open.Values;
+
+namespace DSE.Open.Observations.Abstractions.Tests;
+
+public class YesNoUnsureTests
+{
+    [Theory]
+    [MemberData(nameof(Values))]
+    public void SerializeDeserialize(YesNoUnsure value)
+    {
+        var json = JsonSerializer.Serialize(value);
+        var deserialized = JsonSerializer.Deserialize<YesNoUnsure>(json);
+        Assert.Equal(value, deserialized);
+    }
+
+    [Fact]
+    public void MustBeInitialized()
+    {
+        YesNoUnsure value = default;
+        Assert.Throws<UninitializedValueException<YesNoUnsure, AsciiString>>(() => value.ToString());
+    }
+
+    public static TheoryData<YesNoUnsure> Values { get; } = new TheoryData<YesNoUnsure>() { YesNoUnsure.No, YesNoUnsure.Yes, YesNoUnsure.Unsure, };
+}
