fb39a7721f3fc31e007010a9e888c0249a989e48
Frank Buckley <frank@frankbuckley.com>
2023-08-31 15:06:18 +0100

    YesNo

---
+++ src/DSE.Open.Observations.Abstractions/YesNo.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+using DSE.Open.Values;
+using DSE.Open.Values.Text.Json.Serialization;
+
+namespace DSE.Open.Observations.Abstractions;
+
+/// <summary>
+/// Represents the selection of a choice between "Yes" and "No".
+/// </summary>
+[EquatableValue]
+[JsonConverter(typeof(JsonSpanSerializableValueConverter<YesNo, AsciiString>))]
+[StructLayout(LayoutKind.Auto)]
+public readonly partial struct YesNo : IEquatableValue<YesNo, AsciiString>
+{
+    public static int MaxSerializedCharLength => 3;
+
+    public static bool IsValidValue(AsciiString value)
+    {
+        return value == Yes._value || value == No._value;
+    }   
+
+    public static readonly YesNo Yes = new((AsciiString)"yes", true);
+
+    public static readonly YesNo No = new((AsciiString)"no", true);
+}
---
+++ test/DSE.Open.Observations.Abstractions.Tests/YesNoTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Text.Json;
+using DSE.Open.Values;
+
+namespace DSE.Open.Observations.Abstractions.Tests;
+
+public class YesNoTests
+{
+    [Theory]
+    [MemberData(nameof(Values))]
+    public void SerializeDeserialize(YesNo value)
+    {
+        var json = JsonSerializer.Serialize(value);
+        var deserialized = JsonSerializer.Deserialize<YesNo>(json);
+        Assert.Equal(value, deserialized);
+    }
+
+    [Fact]
+    public void MustBeInitialized()
+    {
+        YesNo value = default;
+        Assert.Throws<UninitializedValueException<YesNo, AsciiString>>(() => value.ToString());
+    }
+
+    public static TheoryData<YesNo> Values { get; } = new TheoryData<YesNo>() { YesNo.No, YesNo.Yes, };
+}
