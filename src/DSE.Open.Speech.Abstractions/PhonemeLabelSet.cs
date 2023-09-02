276c864e0cd0d851fe66e5989779dc6edc936820
Frank Buckley <frank@frankbuckley.com>
2023-09-02 15:36:33 +0100

    Update PhonemeLabelSet.cs

--- src/DSE.Open.Speech.Abstractions/PhonemeLabelSet.cs
+++ src/DSE.Open.Speech.Abstractions/PhonemeLabelSet.cs
@@ -3,6 +3,7 @@
 
 using System.Collections;
 using System.Diagnostics.CodeAnalysis;
+using DSE.Open.Globalization;
 
 namespace DSE.Open.Speech;
 
@@ -11,8 +12,8 @@ namespace DSE.Open.Speech;
 /// </summary>
 public abstract class PhonemeLabelSet : IReadOnlyCollection<PhonemeLabel>
 {
-    private readonly Dictionary<Phoneme, PhonemeLabel> _phonemeLookup = new();
-    private readonly Dictionary<string, PhonemeLabel> _labelLookup = new();
+    private readonly List<PhonemeLabel> _lookup = new();
+    private readonly Dictionary<LanguageTag, Dictionary<string, PhonemeLabel>> _lookup2 = new();
 
     protected PhonemeLabelSet(IEnumerable<PhonemeLabel> notations)
     {
@@ -20,28 +21,55 @@ public abstract class PhonemeLabelSet : IReadOnlyCollection<PhonemeLabel>
 
         foreach (var ps in notations)
         {
-            _phonemeLookup.Add(ps.Phoneme, ps);
-            _labelLookup.Add(ps.Label, ps);
+            _lookup.Add(ps);
+
+            if (!_lookup2.TryGetValue(ps.Language, out var value))
+            {
+                value = new Dictionary<string, PhonemeLabel>(StringComparer.InvariantCulture);
+                _lookup2.Add(ps.Language, value);
+            }
+
+            value.Add(ps.Label, ps);
         }
     }
 
-    public int Count => _phonemeLookup.Count;
+    public int Count => _lookup.Count;
 
-    public bool ContainsPhoneme(Phoneme phoneme) => _phonemeLookup.ContainsKey(phoneme);
+    public bool ContainsLabel(LanguageTag language, string label) => _lookup2.TryGetValue(language, out var lookup) && lookup.ContainsKey(label);
 
-    public bool ContainsLabel(string label) => _labelLookup.ContainsKey(label);
+    public IEnumerable<PhonemeLabel> SelectByLanguage(LanguageTag language)
+    {
+        if (_lookup2.TryGetValue(language, out var lookup))
+        {
+            return lookup.Values;
+        }
 
-    public IEnumerator<PhonemeLabel> GetEnumerator() => _phonemeLookup.Values.GetEnumerator();
+        return Enumerable.Empty<PhonemeLabel>();
+    }
 
-    public bool TryGetForPhoneme(
-        Phoneme key,
+    public IEnumerator<PhonemeLabel> GetEnumerator() => _lookup.GetEnumerator();
+
+    public bool TryGetLookup(
+        LanguageTag language,
+        [MaybeNullWhen(false)] out Dictionary<string, PhonemeLabel> lookup)
+    {
+        if (_lookup2.TryGetValue(language, out lookup)) return true;
+
+        lookup = default;
+        return false;
+    }
+
+    public bool TryGetPhonemeLabel(
+        LanguageTag language,
+        string label,
         [MaybeNullWhen(false)] out PhonemeLabel value)
-        => _phonemeLookup.TryGetValue(key, out value);
+    {
+        if (_lookup2.TryGetValue(language, out var lookup)
+            && lookup.TryGetValue(label, out value)) return true;
 
-    public bool TryGetForSymbol(
-        string key,
-        [MaybeNullWhen(false)] out PhonemeLabel value)
-        => _labelLookup.TryGetValue(key, out value);
+        value = default;
+        return false;
+    }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
