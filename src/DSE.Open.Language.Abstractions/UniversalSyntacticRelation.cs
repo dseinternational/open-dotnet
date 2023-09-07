1a92f3c959a53a0901dd91000d89e259b515989d
Frank Buckley <frank@frankbuckley.com>
2023-09-07 09:16:29 +0100

    Update UniversalSyntacticRelation.cs

--- src/DSE.Open.Language.Abstractions/UniversalSyntacticRelation.cs
+++ src/DSE.Open.Language.Abstractions/UniversalSyntacticRelation.cs
@@ -20,6 +20,10 @@ public readonly partial struct UniversalSyntacticRelation : IEquatableValue<Univ
     {
     }
 
+    private UniversalSyntacticRelation(string value, bool skipValidation) : this((AsciiString)value, skipValidation)
+    {
+    }
+
     public static bool IsValidValue(AsciiString value)
     {
         return !value.IsEmpty
@@ -34,71 +38,71 @@ public readonly partial struct UniversalSyntacticRelation : IEquatableValue<Univ
         return new UniversalSyntacticRelation(value);
     }
 
-    public static readonly UniversalSyntacticRelation ClausalModifierOfNoun = (UniversalSyntacticRelation)"acl";
-    public static readonly UniversalSyntacticRelation RelativeClauseModifier = (UniversalSyntacticRelation)"acl:relcl";
-    public static readonly UniversalSyntacticRelation AdverbialClauseModifier = (UniversalSyntacticRelation)"advcl";
-    public static readonly UniversalSyntacticRelation AdverbialModifier = (UniversalSyntacticRelation)"advmod";
-    public static readonly UniversalSyntacticRelation EmphasizingWord = (UniversalSyntacticRelation)"advmod:emph";
-    public static readonly UniversalSyntacticRelation LocativeAdverbialModifier = (UniversalSyntacticRelation)"advmod:lmod";
-    public static readonly UniversalSyntacticRelation AdjectivalModifier = (UniversalSyntacticRelation)"amod";
-    public static readonly UniversalSyntacticRelation AppositionalModifier = (UniversalSyntacticRelation)"appos";
-    public static readonly UniversalSyntacticRelation Auxiliary = (UniversalSyntacticRelation)"aux";
-    public static readonly UniversalSyntacticRelation PassiveAuxiliary = (UniversalSyntacticRelation)"aux:pass";
-    public static readonly UniversalSyntacticRelation CaseMarking = (UniversalSyntacticRelation)"case";
-    public static readonly UniversalSyntacticRelation CoordinatingConjunction = (UniversalSyntacticRelation)"cc";
-    public static readonly UniversalSyntacticRelation Preconjunct = (UniversalSyntacticRelation)"cc:preconj";
-    public static readonly UniversalSyntacticRelation ClausalComplement = (UniversalSyntacticRelation)"ccomp";
-    public static readonly UniversalSyntacticRelation Classifier = (UniversalSyntacticRelation)"clf";
-    public static readonly UniversalSyntacticRelation Compound = (UniversalSyntacticRelation)"compound";
-    public static readonly UniversalSyntacticRelation LightVerbConstruction = (UniversalSyntacticRelation)"compound:lvc";
-    public static readonly UniversalSyntacticRelation PhrasalVerbParticle = (UniversalSyntacticRelation)"compound:prt";
-    public static readonly UniversalSyntacticRelation ReduplicatedCompounds = (UniversalSyntacticRelation)"compound:redup";
-    public static readonly UniversalSyntacticRelation SerialVerbCompounds = (UniversalSyntacticRelation)"compound:svc";
-    public static readonly UniversalSyntacticRelation Conjunct = (UniversalSyntacticRelation)"conj";
-    public static readonly UniversalSyntacticRelation Copula = (UniversalSyntacticRelation)"cop";
-    public static readonly UniversalSyntacticRelation ClausalSubject = (UniversalSyntacticRelation)"csubj";
-    public static readonly UniversalSyntacticRelation ClausalPassiveSubject = (UniversalSyntacticRelation)"csubj:pass";
-    public static readonly UniversalSyntacticRelation UnspecifiedDependency = (UniversalSyntacticRelation)"dep";
-    public static readonly UniversalSyntacticRelation Determiner = (UniversalSyntacticRelation)"det";
-    public static readonly UniversalSyntacticRelation PronominalQuantifierGoverning = (UniversalSyntacticRelation)"det:numgov";
-    public static readonly UniversalSyntacticRelation PronominalQuantifierAgreeing = (UniversalSyntacticRelation)"det:nummod";
-    public static readonly UniversalSyntacticRelation PossessiveDeterminer = (UniversalSyntacticRelation)"det:poss";
-    public static readonly UniversalSyntacticRelation DiscourseElement = (UniversalSyntacticRelation)"discourse";
-    public static readonly UniversalSyntacticRelation DislocatedElements = (UniversalSyntacticRelation)"dislocated";
-    public static readonly UniversalSyntacticRelation Expletive = (UniversalSyntacticRelation)"expl";
-    public static readonly UniversalSyntacticRelation ImpersonalExpletive = (UniversalSyntacticRelation)"expl:impers";
-    public static readonly UniversalSyntacticRelation ReflexivePronounReflexivePassive = (UniversalSyntacticRelation)"expl:pass";
-    public static readonly UniversalSyntacticRelation ReflexiveCliticWithReflexiveverb = (UniversalSyntacticRelation)"expl:pv";
-    public static readonly UniversalSyntacticRelation FixedMultiwordExpression = (UniversalSyntacticRelation)"fixed";
-    public static readonly UniversalSyntacticRelation FlatMultiwordExpression = (UniversalSyntacticRelation)"flat";
-    public static readonly UniversalSyntacticRelation ForeignWords = (UniversalSyntacticRelation)"flat:foreign";
-    public static readonly UniversalSyntacticRelation Names = (UniversalSyntacticRelation)"flat:name";
-    public static readonly UniversalSyntacticRelation GoesWith = (UniversalSyntacticRelation)"goeswith";
-    public static readonly UniversalSyntacticRelation IndirectObject = (UniversalSyntacticRelation)"iobj";
-    public static readonly UniversalSyntacticRelation List = (UniversalSyntacticRelation)"list";
-    public static readonly UniversalSyntacticRelation Marker = (UniversalSyntacticRelation)"mark";
-    public static readonly UniversalSyntacticRelation NominalModifier = (UniversalSyntacticRelation)"nmod";
-    public static readonly UniversalSyntacticRelation PossessiveNominalModifier = (UniversalSyntacticRelation)"nmod:poss";
-    public static readonly UniversalSyntacticRelation TemporalModifier = (UniversalSyntacticRelation)"nmod:tmod";
-    public static readonly UniversalSyntacticRelation NominalSubject = (UniversalSyntacticRelation)"nsubj";
-    public static readonly UniversalSyntacticRelation PassiveNominalSubject = (UniversalSyntacticRelation)"nsubj:pass";
-    public static readonly UniversalSyntacticRelation NumericModifier = (UniversalSyntacticRelation)"nummod";
-    public static readonly UniversalSyntacticRelation NumericModifierGoverningNoun = (UniversalSyntacticRelation)"nummod:gov";
+    public static readonly UniversalSyntacticRelation ClausalModifierOfNoun = new("acl", true);
+    public static readonly UniversalSyntacticRelation RelativeClauseModifier = new("acl:relcl", true);
+    public static readonly UniversalSyntacticRelation AdverbialClauseModifier = new("advcl", true);
+    public static readonly UniversalSyntacticRelation AdverbialModifier = new("advmod", true);
+    public static readonly UniversalSyntacticRelation EmphasizingWord = new("advmod:emph", true);
+    public static readonly UniversalSyntacticRelation LocativeAdverbialModifier = new("advmod:lmod", true);
+    public static readonly UniversalSyntacticRelation AdjectivalModifier = new("amod", true);
+    public static readonly UniversalSyntacticRelation AppositionalModifier = new("appos", true);
+    public static readonly UniversalSyntacticRelation Auxiliary = new("aux", true);
+    public static readonly UniversalSyntacticRelation PassiveAuxiliary = new("aux:pass", true);
+    public static readonly UniversalSyntacticRelation CaseMarking = new("case", true);
+    public static readonly UniversalSyntacticRelation CoordinatingConjunction = new("cc", true);
+    public static readonly UniversalSyntacticRelation Preconjunct = new("cc:preconj", true);
+    public static readonly UniversalSyntacticRelation ClausalComplement = new("ccomp", true);
+    public static readonly UniversalSyntacticRelation Classifier = new("clf", true);
+    public static readonly UniversalSyntacticRelation Compound = new("compound", true);
+    public static readonly UniversalSyntacticRelation LightVerbConstruction = new("compound:lvc", true);
+    public static readonly UniversalSyntacticRelation PhrasalVerbParticle = new("compound:prt", true);
+    public static readonly UniversalSyntacticRelation ReduplicatedCompounds = new("compound:redup", true);
+    public static readonly UniversalSyntacticRelation SerialVerbCompounds = new("compound:svc", true);
+    public static readonly UniversalSyntacticRelation Conjunct = new("conj", true);
+    public static readonly UniversalSyntacticRelation Copula = new("cop", true);
+    public static readonly UniversalSyntacticRelation ClausalSubject = new("csubj", true);
+    public static readonly UniversalSyntacticRelation ClausalPassiveSubject = new("csubj:pass", true);
+    public static readonly UniversalSyntacticRelation UnspecifiedDependency = new("dep", true);
+    public static readonly UniversalSyntacticRelation Determiner = new("det", true);
+    public static readonly UniversalSyntacticRelation PronominalQuantifierGoverning = new("det:numgov", true);
+    public static readonly UniversalSyntacticRelation PronominalQuantifierAgreeing = new("det:nummod", true);
+    public static readonly UniversalSyntacticRelation PossessiveDeterminer = new("det:poss", true);
+    public static readonly UniversalSyntacticRelation DiscourseElement = new("discourse", true);
+    public static readonly UniversalSyntacticRelation DislocatedElements = new("dislocated", true);
+    public static readonly UniversalSyntacticRelation Expletive = new("expl", true);
+    public static readonly UniversalSyntacticRelation ImpersonalExpletive = new("expl:impers", true);
+    public static readonly UniversalSyntacticRelation ReflexivePronounReflexivePassive = new("expl:pass", true);
+    public static readonly UniversalSyntacticRelation ReflexiveCliticWithReflexiveverb = new("expl:pv", true);
+    public static readonly UniversalSyntacticRelation FixedMultiwordExpression = new("fixed", true);
+    public static readonly UniversalSyntacticRelation FlatMultiwordExpression = new("flat", true);
+    public static readonly UniversalSyntacticRelation ForeignWords = new("flat:foreign", true);
+    public static readonly UniversalSyntacticRelation Names = new("flat:name", true);
+    public static readonly UniversalSyntacticRelation GoesWith = new("goeswith", true);
+    public static readonly UniversalSyntacticRelation IndirectObject = new("iobj", true);
+    public static readonly UniversalSyntacticRelation List = new("list", true);
+    public static readonly UniversalSyntacticRelation Marker = new("mark", true);
+    public static readonly UniversalSyntacticRelation NominalModifier = new("nmod", true);
+    public static readonly UniversalSyntacticRelation PossessiveNominalModifier = new("nmod:poss", true);
+    public static readonly UniversalSyntacticRelation TemporalModifier = new("nmod:tmod", true);
+    public static readonly UniversalSyntacticRelation NominalSubject = new("nsubj", true);
+    public static readonly UniversalSyntacticRelation PassiveNominalSubject = new("nsubj:pass", true);
+    public static readonly UniversalSyntacticRelation NumericModifier = new("nummod", true);
+    public static readonly UniversalSyntacticRelation NumericModifierGoverningNoun = new("nummod:gov", true);
 #pragma warning disable CA1720 // Identifier contains type name
-    public static readonly UniversalSyntacticRelation Object = (UniversalSyntacticRelation)"obj";
+    public static readonly UniversalSyntacticRelation Object = new("obj", true);
 #pragma warning restore CA1720 // Identifier contains type name
-    public static readonly UniversalSyntacticRelation ObliqueNominal = (UniversalSyntacticRelation)"obl";
-    public static readonly UniversalSyntacticRelation AgentModifier = (UniversalSyntacticRelation)"obl:agent";
-    public static readonly UniversalSyntacticRelation ObliqueArgument = (UniversalSyntacticRelation)"obl:arg";
-    public static readonly UniversalSyntacticRelation LocativeModifier = (UniversalSyntacticRelation)"obl:lmod";
-    public static readonly UniversalSyntacticRelation TemporalModifier2 = (UniversalSyntacticRelation)"obl:tmod";
-    public static readonly UniversalSyntacticRelation Orphan = (UniversalSyntacticRelation)"orphan";
-    public static readonly UniversalSyntacticRelation Parataxis = (UniversalSyntacticRelation)"parataxis";
-    public static readonly UniversalSyntacticRelation Punctuation = (UniversalSyntacticRelation)"punct";
-    public static readonly UniversalSyntacticRelation OverriddenDisfluency = (UniversalSyntacticRelation)"reparandum";
-    public static readonly UniversalSyntacticRelation Root = (UniversalSyntacticRelation)"root";
-    public static readonly UniversalSyntacticRelation Vocative = (UniversalSyntacticRelation)"vocative";
-    public static readonly UniversalSyntacticRelation OpenClausalComplement = (UniversalSyntacticRelation)"xcomp";
+    public static readonly UniversalSyntacticRelation ObliqueNominal = new("obl", true);
+    public static readonly UniversalSyntacticRelation AgentModifier = new("obl:agent", true);
+    public static readonly UniversalSyntacticRelation ObliqueArgument = new("obl:arg", true);
+    public static readonly UniversalSyntacticRelation LocativeModifier = new("obl:lmod", true);
+    public static readonly UniversalSyntacticRelation TemporalModifier2 = new("obl:tmod", true);
+    public static readonly UniversalSyntacticRelation Orphan = new("orphan", true);
+    public static readonly UniversalSyntacticRelation Parataxis = new("parataxis", true);
+    public static readonly UniversalSyntacticRelation Punctuation = new("punct", true);
+    public static readonly UniversalSyntacticRelation OverriddenDisfluency = new("reparandum", true);
+    public static readonly UniversalSyntacticRelation Root = new("root", true);
+    public static readonly UniversalSyntacticRelation Vocative = new("vocative", true);
+    public static readonly UniversalSyntacticRelation OpenClausalComplement = new("xcomp", true);
 
     public static readonly IReadOnlySet<UniversalSyntacticRelation> All = FrozenSet.ToFrozenSet(new[]
     {
