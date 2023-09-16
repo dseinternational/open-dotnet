021fc0ea23b5325209b07ab52c4458ce686d7c17
Ryan Buckley <ryan@brownthwaite.com>
2023-09-16 08:58:40 +0100

    Delete obsoleted code

--- src/DSE.Open.Web/UriPathExtensions.cs
+++ src/DSE.Open.Web/UriPathExtensions.cs
@@ -9,23 +9,15 @@ namespace DSE.Open.Web;
 
 public static class UriPathExtensions
 {
-    [Obsolete("Use UriAsciiPath")]
-    public static PathString ToPathString(this AsciiPath path)
-        => path.IsEmpty ? PathString.Empty : new($"/{path}");
-
-    [Obsolete("Use UriAsciiPath")]
-    public static PathString ToLanguagePrefixedPathString(this AsciiPath path, LanguageTag language)
-        => path.IsEmpty ? new($"/{language:L}") : new($"/{language:L}/{path}");
-
     public static PathString ToPathString(this UriPath path)
-        => path.IsEmpty ? PathString.Empty : new($"/{path}");
+        => path.IsEmpty ? PathString.Empty : new PathString($"/{path}");
 
     public static PathString ToLanguagePrefixedPathString(this UriPath path, LanguageTag language)
-        => path.IsEmpty ? new($"/{language:L}") : new($"/{language:L}/{path}");
+        => path.IsEmpty ? new PathString($"/{language:L}") : new PathString($"/{language:L}/{path}");
 
     public static PathString ToPathString(this UriAsciiPath path)
-        => path.IsEmpty ? PathString.Empty : new($"/{path}");
+        => path.IsEmpty ? PathString.Empty : new PathString($"/{path}");
 
     public static PathString ToLanguagePrefixedPathString(this UriAsciiPath path, LanguageTag language)
-        => path.IsEmpty ? new($"/{language:L}") : new($"/{language:L}/{path}");
+        => path.IsEmpty ? new PathString($"/{language:L}") : new PathString($"/{language:L}/{path}");
 }
