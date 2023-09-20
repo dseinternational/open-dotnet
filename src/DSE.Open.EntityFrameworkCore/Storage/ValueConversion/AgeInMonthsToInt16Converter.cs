28ef34aaf9ca60a5a88125c5b14239a011035bdf
Frank Buckley <frank@frankbuckley.com>
2023-09-20 10:29:34 +0100

    AgeInMonthsToInt16/32 Converters

---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/AgeInMonthsToInt16Converter.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using DSE.Open.Values;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class AgeInMonthsToInt16Converter : ValueConverter<AgeInMonths, short>
+{
+    public static readonly AgeInMonthsToInt16Converter Default = new();
+
+    public AgeInMonthsToInt16Converter()
+        : base(c => ConvertToInt16(c), s => ConvertFromInt16(s))
+    {
+    }
+
+    private static short ConvertToInt16(AgeInMonths code) => (short)code.TotalMonths;
+
+    private static AgeInMonths ConvertFromInt16(short code) => new(code);
+}
---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/AgeInMonthsToInt32Converter.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using DSE.Open.Values;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class AgeInMonthsToInt32Converter : ValueConverter<AgeInMonths, int>
+{
+    public static readonly AgeInMonthsToInt32Converter Default = new();
+
+    public AgeInMonthsToInt32Converter()
+        : base(c => ConvertToInt32(c), s => ConvertFromInt32(s))
+    {
+    }
+
+    private static int ConvertToInt32(AgeInMonths code) => code.TotalMonths;
+
+    private static AgeInMonths ConvertFromInt32(int code) => new(code);
+}
