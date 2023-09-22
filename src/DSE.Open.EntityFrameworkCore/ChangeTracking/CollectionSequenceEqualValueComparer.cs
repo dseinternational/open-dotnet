1742c755afcf2931f9a78da02133d543026a50ab
Frank Buckley <frank@frankbuckley.com>
2023-09-22 17:49:29 +0100

    Add some more EF comparers

---
+++ src/DSE.Open.EntityFrameworkCore/ChangeTracking/CollectionSequenceEqualValueComparer.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace DSE.Open.EntityFrameworkCore.ChangeTracking;
+
+public class CollectionSequenceEqualValueComparer<T> : ValueComparer<ICollection<T>>
+{
+    public static readonly CollectionSequenceEqualValueComparer<T> Default = new();
+
+    public CollectionSequenceEqualValueComparer()
+        : base(
+            (c1, c2) => (c1 != null && c2 != null && c1.SequenceEqual(c2)) || (c1 == null && c2 == null),
+            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v!.GetHashCode())),
+            c => c.ToList())
+    {
+    }
+}
---
+++ src/DSE.Open.EntityFrameworkCore/ChangeTracking/ReadOnlyListSequenceEqualValueComparer.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace DSE.Open.EntityFrameworkCore.ChangeTracking;
+
+public sealed class ReadOnlyListSequenceEqualValueComparer<T> : ValueComparer<IReadOnlyList<T>>
+{
+    public static readonly ReadOnlyListSequenceEqualValueComparer<T> Default = new();
+
+    public ReadOnlyListSequenceEqualValueComparer()
+        : base(
+            (c1, c2) => (c1 != null && c2 != null && c1.SequenceEqual(c2)) || (c1 == null && c2 == null),
+            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v!.GetHashCode())),
+            c => c.ToList())
+    {
+    }
+}
