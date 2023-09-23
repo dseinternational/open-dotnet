d1b2ce371d7b295fab1240a2f210d889d569f3b5
Frank Buckley <frank@frankbuckley.com>
2023-09-23 12:06:19 +0100

    Remove UriAsciiPathListToStringCollectionConverter

---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/CollectionToFormattableStringCollectionConverter.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class CollectionToFormattableStringCollectionConverter<T> : ValueConverter<ICollection<T>, IEnumerable<string>>
+    where T : IParsable<T>, IFormattable
+{
+    public static readonly CollectionToFormattableStringCollectionConverter<T> Default = new();
+
+    public CollectionToFormattableStringCollectionConverter() : this(null, null)
+    {
+    }
+
+    public CollectionToFormattableStringCollectionConverter(string? format, IFormatProvider? formatProvider)
+        : base(v => ToCollection(v, format, formatProvider), v => FromCollection(v, formatProvider))
+    {
+        Format = format;
+        FormatProvider = formatProvider;
+    }
+
+    public string? Format { get; }
+
+    public IFormatProvider? FormatProvider { get; }
+
+    private static IEnumerable<string> ToCollection(ICollection<T> set, string? format, IFormatProvider? formatProvider)
+    {
+        Guard.IsNotNull(set);
+        return set.Select(p => p.ToString(format, formatProvider) ?? string.Empty);
+    }
+
+    private static ICollection<T> FromCollection(IEnumerable<string> collection, IFormatProvider? formatProvider)
+    {
+        Guard.IsNotNull(collection);
+        return collection.Select(s => T.Parse(s, formatProvider)).ToList();
+    }
+}
---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/ListToFormattableStringCollectionConverter.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class ListToFormattableStringCollectionConverter<T> : ValueConverter<IList<T>, IEnumerable<string>>
+    where T : IParsable<T>, IFormattable
+{
+    public static readonly ListToFormattableStringCollectionConverter<T> Default = new();
+
+    public ListToFormattableStringCollectionConverter() : this(null, null)
+    {
+    }
+
+    public ListToFormattableStringCollectionConverter(string? format, IFormatProvider? formatProvider)
+        : base(v => ToCollection(v, format, formatProvider), v => FromCollection(v, formatProvider))
+    {
+        Format = format;
+        FormatProvider = formatProvider;
+    }
+
+    public string? Format { get; }
+
+    public IFormatProvider? FormatProvider { get; }
+
+    private static IEnumerable<string> ToCollection(IList<T> set, string? format, IFormatProvider? formatProvider)
+    {
+        Guard.IsNotNull(set);
+        return set.Select(p => p.ToString(format, formatProvider) ?? string.Empty);
+    }
+
+    private static IList<T> FromCollection(IEnumerable<string> collection, IFormatProvider? formatProvider)
+    {
+        Guard.IsNotNull(collection);
+        return collection.Select(s => T.Parse(s, formatProvider)).ToList();
+    }
+}
