3750351238e4ed89ecb52d32f79bc4919b2defe9
Frank Buckley <frank@frankbuckley.com>
2023-10-03 17:10:19 +0100

    Create RequestMetadata.cs

---
+++ src/DSE.Open.Requests/RequestMetadata.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Text.Json.Serialization;
+using DSE.Open.Serialization.DataTransfer;
+using DSE.Open.Sessions;
+
+namespace DSE.Open.Requests;
+
+public record RequestMetadata : ImmutableDataTransferObject
+{
+    [JsonPropertyName("session")]
+    public SessionContext? Session { get; init; }
+
+    [JsonPropertyName("properties")]
+    public IDictionary<string, string> Properties { get; } = new Dictionary<string, string>();
+}
