4e95ee1d317c59cb578db14305a041a5e926f32f
Frank Buckley <frank@dsegroup.org>
2023-10-03 18:23:18 +0100

    Add request/result accessor interfaces

--- src/DSE.Open.Sessions/ISessionContextAccessor.cs
+++ src/DSE.Open.Sessions/ISessionContextAccessor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
 namespace DSE.Open.Sessions;
@@ -8,5 +8,5 @@ public interface ISessionContextAccessor
     /// <summary>
     /// Gets or sets the current session context.
     /// </summary>
-    SessionContext Current { get; set; }
+    SessionContext Current { get; }
 }
