7e3008f825e58e44d7b8cf243ed0451e8b0cb9f5
Frank Buckley <frank@frankbuckley.com>
2023-10-03 17:00:00 +0100

    Create ISessionContextAccessor.cs

---
+++ src/DSE.Open.Sessions/ISessionContextAccessor.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+namespace DSE.Open.Sessions;
+
+public interface ISessionContextAccessor
+{
+    /// <summary>
+    /// Gets or sets the current session context.
+    /// </summary>
+    SessionContext Current { get; set; }
+}
