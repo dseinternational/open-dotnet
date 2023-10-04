b28ca70ca5a62b0ce72a8e2492732d32a2bd02b2
Frank Buckley <frank@frankbuckley.com>
2023-10-04 11:19:09 +0100

    Session RequestResultMetadataExtensions

---
+++ src/DSE.Open.Sessions/RequestResultMetadataExtensions.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using DSE.Open.Requests;
+using DSE.Open.Results;
+
+namespace DSE.Open.Sessions;
+
+public static class RequestResultMetadataExtensions
+{
+    public static SessionContext GetSessionContext(this RequestMetadata requestMetadata)
+    {
+        Guard.IsNotNull(requestMetadata);
+        return (SessionContext)requestMetadata.Properties[SessionContextMetadataKeys.SessionContext];
+    }
+
+    public static SessionContext GetSessionContext(this ResultMetadata resultMetadata)
+    {
+        Guard.IsNotNull(resultMetadata);
+        return (SessionContext)resultMetadata.Properties[SessionContextMetadataKeys.SessionContext];
+    }
+
+    public static bool TryGetSessionContext(this RequestMetadata requestMetadata, out SessionContext? sessionContext)
+    {
+        Guard.IsNotNull(requestMetadata);
+
+        if (requestMetadata.Properties.TryGetValue(SessionContextMetadataKeys.SessionContext, out var sessionObj)
+            && sessionObj is SessionContext session)
+        {
+            sessionContext = session;
+            return true;
+        }
+
+        sessionContext = null;
+        return false;
+    }
+
+    public static bool TryGetSessionContext(this ResultMetadata resultMetadata, out SessionContext? sessionContext)
+    {
+        Guard.IsNotNull(resultMetadata);
+
+        if (resultMetadata.Properties.TryGetValue(SessionContextMetadataKeys.SessionContext, out var sessionObj)
+            && sessionObj is SessionContext session)
+        {
+            sessionContext = session;
+            return true;
+        }
+
+        sessionContext = null;
+        return false;
+    }
+}
---
+++ src/DSE.Open.Sessions/SessionContextMetadataKeys.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+namespace DSE.Open.Sessions;
+
+public static class SessionContextMetadataKeys
+{
+    public const string SessionContext = "DSE-Session-Context";
+}
