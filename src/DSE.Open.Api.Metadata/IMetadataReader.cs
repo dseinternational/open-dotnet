0ffcb4f539d06c840736b1de84f69c8208ddca82
Frank Buckley <frank@frankbuckley.com>
2023-10-05 10:03:45 +0100

    Add to reader/writer

--- src/DSE.Open.Api.Metadata/IMetadataReader.cs
+++ src/DSE.Open.Api.Metadata/IMetadataReader.cs
@@ -8,7 +8,31 @@ namespace DSE.Open.Api.Metadata;
 
 public interface IMetadataReader
 {
+    /// <summary>
+    /// Reads metadata from the source key value pairs and sets the corresponding
+    /// values in the specified <paramref name="request"/> metadata.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="result"></param>
+    /// <param name="source"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     ValueTask ReadRequestMetadataAsync(
+        RequestMetadata request,
+        ResultMetadata result,
+        IDictionary<string, string> source,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Reads metadata from the source key value pairs and sets the corresponding
+    /// values in the specified <paramref name="result"/> metadata.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="result"></param>
+    /// <param name="source"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask ReadResultMetadataAsync(
         RequestMetadata request,
         ResultMetadata result,
         IDictionary<string, string> source,
--- src/DSE.Open.Api.Metadata/IMetadataWriter.cs
+++ src/DSE.Open.Api.Metadata/IMetadataWriter.cs
@@ -8,7 +8,31 @@ namespace DSE.Open.Api.Metadata;
 
 public interface IMetadataWriter
 {
+    /// <summary>
+    /// Writes key value pairs to the specified dictionary representing the data in
+    /// the specified <paramref name="request"/> metadata.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="result"></param>
+    /// <param name="target"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     ValueTask WriteResultMetadataAsync(
+        RequestMetadata request,
+        ResultMetadata result,
+        IDictionary<string, string> target,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Writes key value pairs to the specified dictionary representing the data in
+    /// the specified <paramref name="result"/> metadata.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="result"></param>
+    /// <param name="target"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask WriteRequestMetadataAsync(
         RequestMetadata request,
         ResultMetadata result,
         IDictionary<string, string> target,
