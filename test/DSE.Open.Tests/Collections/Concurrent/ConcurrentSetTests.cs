b8c23866fab94d256ab49e8e4cd5471e48a05ca8
Frank Buckley <frank@frankbuckley.com>
2023-11-10 13:20:45 +0000

    Update ConcurrentSetTests.cs

--- test/DSE.Open.Tests/Collections/Concurrent/ConcurrentSetTests.cs
+++ test/DSE.Open.Tests/Collections/Concurrent/ConcurrentSetTests.cs
@@ -2,6 +2,7 @@
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
 using DSE.Open.Collections.Concurrent;
+using DSE.Open.Collections.Generic;
 
 namespace DSE.Open.Tests.Collections.Concurrent;
 
@@ -19,8 +20,38 @@ public class ConcurrentSetTests
             _ = set.Add(i);
             Assert.True(set.Contains(i), $"Failed added: {i}");
 
-            set.TryRemove(i);
+            _ = set.TryRemove(i);
             Assert.False(set.Contains(i), $"Failed removed: {i}");
         });
+    }
+
+    [Fact]
+    public void Iterate()
+    {
+        var set = new ConcurrentSet<int>();
+
+        var values = Enumerable.Range(0, 1000);
+
+        set.AddRange(values);
+
+        _ = Parallel.ForEach(values, (i) =>
+        {
+            foreach (var value in set)
+            {
+                Assert.True(set.Contains(value), $"Failed: {i}");
+            }
+        });
+    }
+
+    [Fact]
+    public void AddRange()
+    {
+        var set = new ConcurrentSet<int>();
+
+        var values = Enumerable.Range(0, 1000);
+
+        set.AddRange(values);
+
+        Assert.Equal(1000, set.Count);
     }
 }
