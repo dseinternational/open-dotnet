7c12885c5f35857312216d50930f7b60546bd8e5
Frank Buckley <frank@frankbuckley.com>
2023-11-25 14:48:41 +0000

    Create Comparisons.cs

---
+++ src/DSE.Open/Comparisons.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+namespace DSE.Open;
+
+public static class Comparisons
+{
+    /// <summary>
+    /// Returns the last of two comparable values.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static T Max<T>(T a, T b) where T : IComparable<T>
+    {
+        return a.CompareTo(b) >= 0 ? a : b;
+    }
+
+    /// <summary>
+    /// Returns the first of two comparable values.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static T Min<T>(T a, T b) where T : IComparable<T>
+    {
+        return a.CompareTo(b) <= 0 ? a : b;
+    }
+}
