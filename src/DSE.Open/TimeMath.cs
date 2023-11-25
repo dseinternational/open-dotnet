99f5749a091b2e0e0d6fee0ab6b4ac3815c30bd4
Frank Buckley <frank@frankbuckley.com>
2023-11-25 14:48:43 +0000

    Create TimeMath.cs

---
+++ src/DSE.Open/TimeMath.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+namespace DSE.Open;
+
+public static class TimeMath
+{
+    /// <summary>
+    /// Returns the latest of two <see cref="DateTime"/> values.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static DateTime Max(DateTime a, DateTime b)
+    {
+        return a >= b ? a : b;
+    }
+
+    /// <summary>
+    /// Returns the latest of two <see cref="DateTimeOffset"/> values.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static DateTimeOffset Max(DateTimeOffset a, DateTimeOffset b)
+    {
+        return a >= b ? a : b;
+    }
+
+    /// <summary>
+    /// Returns the longest of two <see cref="TimeSpan"/> values.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static TimeSpan Max(TimeSpan a, TimeSpan b)
+    {
+        return a >= b ? a : b;
+    }
+
+    /// <summary>
+    /// Returns the earliest of two <see cref="DateTime"/> values.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static DateTime Min(DateTime a, DateTime b)
+    {
+        return a <= b ? a : b;
+    }
+
+    /// <summary>
+    /// Returns the earliest of two <see cref="DateTimeOffset"/> values.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static DateTimeOffset Min(DateTimeOffset a, DateTimeOffset b)
+    {
+        return a <= b ? a : b;
+    }
+
+    /// <summary>
+    /// Returns the shortest of two <see cref="TimeSpan"/> values.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static TimeSpan Min(TimeSpan a, TimeSpan b)
+    {
+        return a <= b ? a : b;
+    }
+}
