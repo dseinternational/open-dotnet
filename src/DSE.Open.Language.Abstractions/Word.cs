edb60c434034ac163917aaf0114c39df53e39fa6
Frank Buckley <frank@frankbuckley.com>
2023-11-30 09:36:04 +0000

    Support templated word

--- src/DSE.Open.Language.Abstractions/Word.cs
+++ src/DSE.Open.Language.Abstractions/Word.cs
@@ -13,9 +13,12 @@ namespace DSE.Open.Language;
 /// (or sometimes alone) to form a sentence and typically shown with a space on
 /// either side when written or printed. Includes compound words, which may be
 /// hyphenated, space-separated or not separated.
+/// <para>May also be template - for example, as a placeholder for a proper
+/// noun. Templates are in the format <c>{{ template_id }}</c> where <c>template_id</c>
+/// is from 4 to 26 lowercase ASCII characters or underscores ('_').</para>
 /// </summary>
 /// <remarks>
-/// This structure is simply the characters in the word. For tokens, see TokenizedWord.
+/// This structure is simply the characters in the word or template. For tokens, see TokenizedWord.
 /// </remarks>
 [ComparableValue]
 [StructLayout(LayoutKind.Auto)]
@@ -24,17 +27,68 @@ public readonly partial struct Word
     : IComparableValue<Word, CharSequence>,
       ISpanSerializableValue<Word, CharSequence>
 {
-    public static int MaxSerializedCharLength => 64;
+    public static int MaxSerializedCharLength => 32;
 
     public Word(string word) : this((CharSequence)word)
     {
     }
+
+    public CharSequence Value => _value;
+
+    public bool IsTemplate => !_value.IsEmpty && _value[0] == '{';
 
     public static bool IsValidValue(CharSequence value)
     {
         if (value.IsEmpty || value.Length > MaxSerializedCharLength)
         {
             return false;
+        }
+
+        // template "{{ child_subject_given_name }}"
+        // template id min 4 chars, max 26 chars
+        // with spaces inside braces, max overall length = 32
+
+        if (value[0] == '{')
+        {
+            if (value.Length < 8)
+            {
+                return false;
+            }
+
+            if (value[1] != '{'
+                || value[^1] != '}'
+                || value[^2] != '}')
+            {
+                return false;
+            }
+
+            for (var i = 2; i < value.Length - 2; i++)
+            {
+                var c = value[i];
+
+                if (char.IsWhiteSpace(c))
+                {
+                    if (i == 2 || i == value.Length - 3)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                if (char.IsAsciiLetterLower(c) || c == '_')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         for (var i = 0; i < value.Length; i++)
--- test/DSE.Open.Language.Abstractions.Tests/WordTests.cs
+++ test/DSE.Open.Language.Abstractions.Tests/WordTests.cs
@@ -28,6 +28,8 @@ public class WordTests
     [InlineData("ice-cream")]
     [InlineData("ice cream")]
     [InlineData("teachers'")]
+    [InlineData("{{ child_name }}")]
+    [InlineData("{{child_name}}")]
     public void ParseSucceedsIfValid(string signValue)
     {
         _ = Word.Parse(signValue, CultureInfo.InvariantCulture);
@@ -41,8 +43,34 @@ public class WordTests
     [InlineData("'ball")]
     [InlineData("b:at")]
     [InlineData("")]
+    [InlineData("{{  child_name  }}")]
+    [InlineData("{{CHILD_NAME}}")]
     public void ParseFailsIfInvalid(string signValue)
     {
         Assert.Throws<FormatException>(() => Word.Parse(signValue, CultureInfo.InvariantCulture));
     }
+
+    [Theory]
+    [InlineData("{{ child_name }}")]
+    [InlineData("{{child_name}}")]
+    public void TemplatesAreIdentifiedAsTemplates(string signValue)
+    {
+        var w = Word.Parse(signValue, CultureInfo.InvariantCulture);
+        Assert.True(w.IsTemplate);
+    }
+
+    [Theory]
+    [InlineData("television")]
+    [InlineData("ball")]
+    [InlineData("bat")]
+    [InlineData("Emma's")]
+    [InlineData("ice-cream")]
+    [InlineData("ice cream")]
+    [InlineData("teachers'")]
+    public void NonTemplatesAreNotIdentifiedAsTemplates(string signValue)
+    {
+        var w = Word.Parse(signValue, CultureInfo.InvariantCulture);
+        Assert.False(w.IsTemplate);
+    }
+
 }
