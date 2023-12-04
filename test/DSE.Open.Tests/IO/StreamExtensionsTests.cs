ffbc772244258f45b7b2a3d8c6abfa68a45fca31
Ryan Buckley <ryan@brownthwaite.com>
2023-12-04 18:51:53 +0000

    Ensure StreamReader is not disposed before reading string in StreamExtensions
    I'm not sure its _actually_ possible to dispose before completing here, but better safe than sorry.

---
+++ test/DSE.Open.Tests/IO/StreamExtensionsTests.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Text;
+using DSE.Open.IO;
+
+namespace DSE.Open.Tests.IO;
+
+public class StreamExtensionsTests
+{
+    [Fact]
+    public async Task ReadToEndAsStringAsync_ReadsCorrectly()
+    {
+        var stream = new MemoryStream(1024);
+        var bytes = "Hello, World!"u8.ToArray();
+        await stream.WriteAsync(bytes);
+        stream.Position = 0;
+
+        var result = await stream.ReadToEndAsStringAsync();
+
+        Assert.Equal("Hello, World!", result);
+        Assert.Throws<ObjectDisposedException>(() => stream.Write("Hi!"u8.ToArray()));
+    }
+}
