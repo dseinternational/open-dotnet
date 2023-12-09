1751b6f9fdc4ac059909018df5d9f4aed2cd87e3
Frank Buckley <frank@frankbuckley.com>
2023-12-09 11:46:30 +0000

    Merge semantic relation tags

--- src/DSE.Open.Language.Annotations/SemanticRelationTag.cs
+++ src/DSE.Open.Language.Annotations/SemanticRelationTag.cs
@@ -11,18 +11,20 @@ namespace DSE.Open.Language.Annotations;
 
 [EquatableValue]
 [StructLayout(LayoutKind.Auto)]
-[JsonConverter(typeof(JsonUtf8SpanSerializableValueConverter<TwoWordPhraseType, AsciiString>))]
-public readonly partial struct TwoWordPhraseType : IEquatableValue<TwoWordPhraseType, AsciiString>, IUtf8SpanSerializable<TwoWordPhraseType>
+[JsonConverter(typeof(JsonUtf8SpanSerializableValueConverter<SemanticRelationTag, AsciiString>))]
+public readonly partial struct SemanticRelationTag
+    : IEquatableValue<SemanticRelationTag, AsciiString>,
+      IUtf8SpanSerializable<SemanticRelationTag>
 {
     public static int MaxSerializedCharLength => 32;
 
     public static int MaxSerializedByteLength => 32;
 
-    public TwoWordPhraseType(string value) : this((AsciiString)value)
+    public SemanticRelationTag(string value) : this((AsciiString)value)
     {
     }
 
-    private TwoWordPhraseType(string value, bool skipValidation) : this((AsciiString)value, skipValidation)
+    private SemanticRelationTag(string value, bool skipValidation) : this((AsciiString)value, skipValidation)
     {
     }
 
@@ -34,89 +36,114 @@ public readonly partial struct TwoWordPhraseType : IEquatableValue<TwoWordPhrase
     }
 
 #pragma warning disable CA2225 // Operator overloads have named alternates - Parse
-    public static explicit operator TwoWordPhraseType(string value)
+    public static explicit operator SemanticRelationTag(string value)
 #pragma warning restore CA2225 // Operator overloads have named alternates
     {
-        return new TwoWordPhraseType(value);
+        return new SemanticRelationTag(value);
     }
+
 
     /// <summary>
     /// Indicates a <b>noun + verb</b> <c>[(NOUN|PROPN)+VERB]</c> (agent + action)
     /// construction - for example "dog bark", "baby eat".
     /// </summary>
-    public static readonly TwoWordPhraseType AgentAction = new("agent-action", true);
+    public static readonly SemanticRelationTag AgentAction = new("agent-action", true);
 
     /// <summary>
     /// Indicates a <b>verb + noun</b> <c>[VERB+(NOUN|PROPN)]</c> (action + object)
     /// construction - for example "throw ball", "drink juice", "washing cat".
     /// </summary>
-    public static readonly TwoWordPhraseType ActionObject = new("action-object", true);
+    public static readonly SemanticRelationTag ActionObject = new("action-object", true);
 
     /// <summary>
     /// Indicates a <b>verb + noun</b> <c>[(NOUN|PROPN)+NOUN]</c> (agent + object)
     /// construction - for example "daddy toy" (daddy get the toy),
     /// "baby ball" (give the baby the ball).
     /// </summary>
-    public static readonly TwoWordPhraseType AgentObject = new("agent-object", true);
+    public static readonly SemanticRelationTag AgentObject = new("agent-object", true);
 
     /// <summary>
     /// Indicates a <b>noun + possessive + noun</b> <c>[NOUN+PART+NOUN]</c> (possessive)
     /// construction - for example "daddy car" (daddy's car), "dog bowl" (dog's bowl).
     /// </summary>
-    public static readonly TwoWordPhraseType Possessive = new("possessive", true);
+    public static readonly SemanticRelationTag Possessive = new("possessive", true);
 
     /// <summary>
     /// Indicates an <b>adjective + noun</b> or <b>noun + adjective</b>
     /// <c>ADJ+NOUN|NOUN+ADJ</c> (descriptive) construction - for example
     /// "big truck", "blue ball", "dog wet", "shoe dirty".
     /// </summary>
-    public static readonly TwoWordPhraseType Descriptive = new("descriptive", true);
+    public static readonly SemanticRelationTag Descriptive = new("descriptive", true);
 
     /// Indicates a <b>preposition + noun</b> or <b>noun + preposition</b>
     /// <c>[ADP+NOUN]</c> (locative, position/place) construction - for example
     /// "on chair", "in kitchen", "under table".
-    public static readonly TwoWordPhraseType Locative = new("locative", true);
+    public static readonly SemanticRelationTag Locative = new("locative", true);
 
     /// <summary>
     /// Indicates an <b>verb + adverb</b> <c>[VERB+ADV]</c> (temporal)
     /// construction - for example "drink" (I want to drink now),
     /// "eat later".
     /// </summary>
-    public static readonly TwoWordPhraseType Temporal = new("temporal", true);
+    public static readonly SemanticRelationTag Temporal = new("temporal", true);
 
     /// <summary>
     /// Indicates a <b>number + noun</b> <c>[NUM+NOUN]</c> (quantitative)
     /// construction - for example "two balls", "three bags".
     /// </summary>
-    public static readonly TwoWordPhraseType Quantitative = new("temporal", true);
+    public static readonly SemanticRelationTag Quantitative = new("temporal", true);
 
     /// <summary>
     /// Indicates a <b>noun + noun</b> <c>[NOUN+NOUN]</c> construction where the
     /// two nouns are not in a possessive relationship but are commonly associated
     /// with each other - for example "sock shoe", "bowl spoon".
     /// </summary>
-    public static readonly TwoWordPhraseType Conjunctive = new("conjunctive", true);
+    public static readonly SemanticRelationTag Conjunctive = new("conjunctive", true);
 
     /// <summary>
     /// Indicates a <b>determiner + noun</b> <c>DET+NOUN</c> (existence)
     /// construction  - for example, "that ball", "this toy".
     /// </summary>
-    public static readonly TwoWordPhraseType Existence = new("existence", true);
+    public static readonly SemanticRelationTag Existence = new("existence", true);
 
     /// <summary>
     /// Indicates a <b>adjective + noun</b> <c>[ADJ+NOUN]</c> (recurrence)
     /// construction that expresses the idea of repetition or something happening
     /// again - for example "another ball", "more juice".
     /// </summary>
-    public static readonly TwoWordPhraseType Recurrence = new("recurrence", true);
+    public static readonly SemanticRelationTag Recurrence = new("recurrence", true);
 
-    public static readonly TwoWordPhraseType NonExistence = new("non-existence", true);
+    public static readonly SemanticRelationTag NonExistence = new("non-existence", true);
 
-    public static readonly TwoWordPhraseType Rejection = new("rejection", true);
+    public static readonly SemanticRelationTag Rejection = new("rejection", true);
 
-    public static readonly TwoWordPhraseType Denial = new("denial", true);
+    public static readonly SemanticRelationTag Denial = new("denial", true);
 
-    public static readonly FrozenSet<TwoWordPhraseType> All = FrozenSet.ToFrozenSet(
+    /// <summary>
+    /// Indicates a <b>noun + verb + noun</b> <c>[(NOUN|PROPN)+VERB+(NOUN|PROPN)]</c>
+    /// (agent - action - object)
+    /// construction - for example "dog bark", "baby eat".
+    /// </summary>
+    public static readonly SemanticRelationTag AgentActionObject = new("agent-action-object", true);
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static readonly SemanticRelationTag AgentActionLocative = new("agent-action-locative", true);
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static readonly SemanticRelationTag AgentObjectLocative = new("agent-object-locative", true);
+
+    /// <summary>
+    /// <c>[NOUN+ADP+NOUN|VERB+ADP+NOUN]</c> "toy in box", "hide under bed"
+    /// </summary>
+    public static readonly SemanticRelationTag Preposition = new("preposition", true);
+
+    // TODO: complete
+
+    public static readonly FrozenSet<SemanticRelationTag> All = FrozenSet.ToFrozenSet(
     [
         ActionObject,
         AgentAction,
@@ -132,6 +159,10 @@ public readonly partial struct TwoWordPhraseType : IEquatableValue<TwoWordPhrase
         Recurrence,
         Rejection,
         Temporal,
+        AgentActionObject,
+        AgentActionLocative,
+        AgentObjectLocative,
+        Preposition,
     ]);
 
     private static readonly FrozenSet<AsciiString> s_validValues =
--- test/DSE.Open.Language.Annotations.Tests/SemanticRelationTagTests.cs
+++ test/DSE.Open.Language.Annotations.Tests/SemanticRelationTagTests.cs
@@ -5,13 +5,13 @@ using System.Text.Json;
 
 namespace DSE.Open.Language.Annotations;
 
-public class TwoWordPhraseTypeTests
+public class SemanticRelationTagTests
 {
     [Theory]
     [MemberData(nameof(Tags))]
     public void Parse_valid_tags(string tagStr)
     {
-        var tag = TwoWordPhraseType.Parse(tagStr, CultureInfo.InvariantCulture);
+        var tag = SemanticRelationTag.Parse(tagStr, CultureInfo.InvariantCulture);
         Assert.NotEqual(default, tag);
     }
 
@@ -19,9 +19,9 @@ public class TwoWordPhraseTypeTests
     [MemberData(nameof(Tags))]
     public void Serialize_deserialize(string tagStr)
     {
-        var tag = TwoWordPhraseType.Parse(tagStr, CultureInfo.InvariantCulture);
+        var tag = SemanticRelationTag.Parse(tagStr, CultureInfo.InvariantCulture);
         var json = JsonSerializer.Serialize(tag);
-        var deserialized = JsonSerializer.Deserialize<TwoWordPhraseType>(json);
+        var deserialized = JsonSerializer.Deserialize<SemanticRelationTag>(json);
         Assert.Equal(tag, deserialized);
     }
 
