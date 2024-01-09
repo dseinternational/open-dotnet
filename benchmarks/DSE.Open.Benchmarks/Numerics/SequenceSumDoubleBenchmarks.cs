0f73f1d9720349871b206825fa1ddb745c8a4960
Frank Buckley <frank@dsegroup.org>
2024-01-09 18:02:26 +0000

    Update SequenceSumDoubleBenchmarks.cs

--- benchmarks/DSE.Open.Benchmarks/Numerics/SequenceSumDoubleBenchmarks.cs
+++ benchmarks/DSE.Open.Benchmarks/Numerics/SequenceSumDoubleBenchmarks.cs
@@ -1,6 +1,7 @@
-﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
+using System.Collections.ObjectModel;
 using BenchmarkDotNet.Attributes;
 
 namespace DSE.Open.Benchmarks.Numerics;
