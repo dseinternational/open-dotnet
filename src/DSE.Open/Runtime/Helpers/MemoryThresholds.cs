68a8d712982fff22a702a97ed2b0ee624f7c819c
Frank Buckley <frank@frankbuckley.com>
2024-01-26 17:10:32 +0000

    Create MemoryThresholds.cs

---
+++ src/DSE.Open/Runtime/Helpers/MemoryThresholds.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+
+namespace DSE.Open.Runtime.Helpers;
+
+/// <summary>
+/// Provides thresholds for stack memory allocation.
+/// </summary>
+public static class MemoryThresholds
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool CanStackalloc<T>(int length)
+    {
+        return Unsafe.SizeOf<T>() * length <= StackallocByteThreshold;
+    }
+
+    public const int StackallocByteThreshold = 256;
+
+    public const int StackallocCharThreshold = StackallocByteThreshold / 2;
+}
