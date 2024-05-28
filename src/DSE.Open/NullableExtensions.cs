0cfa59fe50ee42322f6876f7b4362e4dcc2628fb
Frank Buckley <frank@frankbuckley.com>
2024-05-28 11:48:41 +0100

    Add NullableExtensions.IsNullOrDefault<T>()

---
+++ src/DSE.Open/NullableExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace DSE.Open;
+
+public static class NullableExtensions
+{
+    public static bool IsNullOrDefault<T>([NotNullWhen(true)] this T? nullable)
+        where T : struct, IEquatable<T>
+    {
+        return !nullable.HasValue || nullable.Value.Equals(default);
+    }
+}
---
+++ test/DSE.Open.Tests/NullableExtensionsTests.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+namespace DSE.Open.Tests;
+
+public class NullableExtensionsTests
+{
+    [Fact]
+    public void IsNullOrDefaultTrueForDefaultInt32()
+    {
+        int? i = 0;
+        Assert.True(i.IsNullOrDefault());
+    }
+
+    [Fact]
+    public void IsNullOrDefaultTrueForNullInt32()
+    {
+        int? i = null;
+        Assert.True(i.IsNullOrDefault());
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(-1)]
+    [InlineData(int.MaxValue)]
+    [InlineData(int.MinValue)]
+    public void IsNullOrDefaultFalseForNonZeroInt32(int value)
+    {
+        int? i = value;
+        Assert.False(i.IsNullOrDefault());
+    }
+
+    [Fact]
+    public void IsNullOrDefaultTrueForEmptyGuid()
+    {
+        Guid? value = Guid.Empty;
+        Assert.True(value.IsNullOrDefault());
+    }
+}
