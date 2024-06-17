d448c5f5d46e695975eefc3ea2f2b755788d917e
Ryan Buckley <ryan@brownthwaite.com>
2024-06-17 09:17:20 +0100

    Extend tests for SessionContextSerializer
    Closes #52

--- test/DSE.Open.Sessions.Tests/SessionContextSerializerTests.cs
+++ test/DSE.Open.Sessions.Tests/SessionContextSerializerTests.cs
@@ -10,9 +10,13 @@ public class SessionContextSerializerTests
     [Fact]
     public void SerializeDeserializeUtf8Json()
     {
-        var context = new SessionContext();
-
-        context.StorageTokens["test"] = "NbyaXkkUbDhmv6TSnPN1Himt5AukzZsAH22wsAISPXI74YhS8VKreWriNYAzAF80";
+        var context = new SessionContext
+        {
+            StorageTokens =
+            {
+                ["test"] = "NbyaXkkUbDhmv6TSnPN1Himt5AukzZsAH22wsAISPXI74YhS8VKreWriNYAzAF80"
+            }
+        };
 
         var utf8Json = SessionContextSerializer.SerializeToUtf8Json(context);
         var deserialized = SessionContextSerializer.DeserializeFromUtf8Json(utf8Json.Span);
@@ -20,7 +24,8 @@ public class SessionContextSerializerTests
     }
 
     [Theory]
-    [InlineData("eyJpZCI6InNlc3NfbHB0bFd5bnJjajN5Vk1xcHBkMzZCelh3aXBsZE0wRmlaTFF2M0diNk02ZU10c0NkIiwic3RvcmFnZV90b2tlbnMiOnt9LCJjcmVhdGVkIjoiMjAyMy0xMC0wNlQxMzoyMTozMS4yODI1MTIzKzAwOjAwIn0=")]
+    [InlineData(
+        "eyJpZCI6InNlc3NfbHB0bFd5bnJjajN5Vk1xcHBkMzZCelh3aXBsZE0wRmlaTFF2M0diNk02ZU10c0NkIiwic3RvcmFnZV90b2tlbnMiOnt9LCJjcmVhdGVkIjoiMjAyMy0xMC0wNlQxMzoyMTozMS4yODI1MTIzKzAwOjAwIn0=")]
     public void TryDeserializeUtf8Json(string base64)
     {
         var succeeded = SessionContextSerializer.TryDeserializeFromBase64Utf8Json(base64, out var sessionContext);
@@ -28,4 +33,47 @@ public class SessionContextSerializerTests
         Assert.True(succeeded);
         Assert.NotNull(sessionContext);
     }
+
+    [Fact]
+    public void SerializeToBase64Utf8Json_ShouldRoundtrip()
+    {
+        // Arrange
+        var context = new SessionContext
+        {
+            StorageTokens =
+            {
+                ["test"] = "NbyaXkkUbDhmv6TSnPN1Himt5AukzZsAH22wsAISPXI74YhS8VKreWriNYAzAF80"
+            }
+        };
+
+        // Act
+        var base64 = SessionContextSerializer.SerializeToBase64Utf8Json(context);
+        var deserialized = SessionContextSerializer.DeserializeFromBase64Utf8Json(base64);
+
+        // Assert
+        Assert.Equivalent(context, deserialized);
+    }
+
+    [Fact]
+    public void TryDeserializeFromBase64Utf8Json_WithValidValue_ShouldReturnTrue()
+    {
+        // Arrange
+        var context = new SessionContext
+        {
+            StorageTokens =
+            {
+                ["test"] = "NbyaXkkUbDhmv6TSnPN1Himt5AukzZsAH22wsAISPXI74YhS8VKreWriNYAzAF80"
+            }
+        };
+
+        // Act
+        var base64 = SessionContextSerializer.SerializeToBase64Utf8Json(context);
+        var result = SessionContextSerializer.TryDeserializeFromBase64Utf8Json(base64, out var sessionContext);
+
+        // Assert
+        Assert.True(result);
+        Assert.NotNull(sessionContext);
+        Assert.Equivalent(context, sessionContext);
+    }
+
 }
