f38f9f6689c04b0f5f3f439ca7c6d6cbece84ebb
Frank Buckley <frank@frankbuckley.com>
2024-07-26 11:52:31 +0100

    Add AmountToDecimalConverter

---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/AmountToDecimalConverter.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using DSE.Open.Values;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class AmountToDecimalConverter : ValueConverter<Amount, decimal>
+{
+    public static readonly AmountToDecimalConverter Default = new();
+
+    public AmountToDecimalConverter() : base(c => ConvertTo(c), s => ConvertFrom(s))
+    {
+    }
+
+    private static decimal ConvertTo(Amount value)
+    {
+        return value;
+    }
+
+    private static Amount ConvertFrom(decimal value)
+    {
+        if (Amount.TryFromValue(value, out var result))
+        {
+            return result;
+        }
+
+        ValueConversionException.Throw($"Could not convert value {value} to {nameof(Amount)}");
+        return default; // unreachable
+    }
+}
