8c598b418190558f83b787c706e152d3f1a2bec1
Frank Buckley <frank@frankbuckley.com>
2024-07-27 14:43:19 +0100

    Create NumberCollectionExtensions.cs

---
+++ src/DSE.Open/Collections/Generic/NumberCollectionExtensions.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Numerics;
+
+namespace DSE.Open.Collections.Generic;
+
+public static class NumberCollectionExtensions
+{
+    public static IEnumerable<TTo> ConvertChecked<TFrom, TTo>(this IEnumerable<TFrom> collection)
+        where TFrom : INumberBase<TFrom>
+        where TTo : INumberBase<TTo>
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+
+        foreach (var value in collection)
+        {
+            yield return TTo.CreateChecked(value);
+        }
+    }
+
+    public static IEnumerable<TTo> ConvertSaturating<TFrom, TTo>(this IEnumerable<TFrom> collection)
+        where TFrom : INumberBase<TFrom>
+        where TTo : INumberBase<TTo>
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+
+        foreach (var value in collection)
+        {
+            yield return TTo.CreateSaturating(value);
+        }
+    }
+
+    public static IEnumerable<TTo> ConvertTruncating<TFrom, TTo>(this IEnumerable<TFrom> collection)
+        where TFrom : INumberBase<TFrom>
+        where TTo : INumberBase<TTo>
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+
+        foreach (var value in collection)
+        {
+            yield return TTo.CreateTruncating(value);
+        }
+    }
+}
