b6d85c82b6252703358c7497e93e7596a4a35e6c
Frank Buckley <frank@frankbuckley.com>
2024-07-27 14:58:09 +0100

    Create NumberConverter.cs

---
+++ src/DSE.Open.EntityFrameworkCore/Storage/ValueConversion/NumberConverter.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Numerics;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DSE.Open.EntityFrameworkCore.Storage.ValueConversion;
+
+public sealed class NumberConverter<TFrom, TTo> : ValueConverter<TFrom, TTo>
+    where TFrom : INumberBase<TFrom>
+    where TTo : INumberBase<TTo>
+{
+    public static readonly NumberConverter<TFrom, TTo> Default = new();
+
+    public NumberConverter() : base(c => ConvertTo(c), s => ConvertFrom(s))
+    {
+    }
+
+    private static TTo ConvertTo(TFrom value)
+    {
+        return TTo.CreateChecked(value);
+    }
+
+    private static TFrom ConvertFrom(TTo value)
+    {
+        return TFrom.CreateChecked(value);
+    }
+}
