6deea3a02c2b6ae8220c15504fafb248858c4d4c
Frank Buckley <frank@frankbuckley.com>
2024-07-31 10:12:51 +0100

    Update ObservationSnapshot.cs

--- src/DSE.Open.Observations.Abstractions/ObservationSnapshot.cs
+++ src/DSE.Open.Observations.Abstractions/ObservationSnapshot.cs
@@ -9,15 +9,12 @@ namespace DSE.Open.Observations;
 /// <summary>
 /// Records an observation at a point in time.
 /// </summary>
-public abstract record ObservationSnapshot<TObs, TValue>
-    where TObs : Observation<TValue>
-    where TValue : IEquatable<TValue>
+public abstract record ObservationSnapshot
 {
-    protected ObservationSnapshot(DateTimeOffset time, TObs observation)
+    protected ObservationSnapshot(DateTimeOffset time)
     {
         ObservationsValidator.EnsureMinimumObservationTime(time);
         Time = time;
-        Observation = observation;
     }
 
     /// <summary>
@@ -28,6 +25,24 @@ public abstract record ObservationSnapshot<TObs, TValue>
     public DateTimeOffset Time { get; private init; }
 
     /// <summary>
+    /// Gets a code that discriminates between measurement types and is suitable for use as a hash code.
+    /// </summary>
+    public abstract int GetMeasurementHashCode();
+}
+
+/// <summary>
+/// Records an observation at a point in time.
+/// </summary>
+public abstract record ObservationSnapshot<TObs, TValue> : ObservationSnapshot
+    where TObs : Observation<TValue>
+    where TValue : IEquatable<TValue>
+{
+    protected ObservationSnapshot(DateTimeOffset time, TObs observation) : base(time)
+    {
+        Observation = observation;
+    }
+
+    /// <summary>
     /// The observation at the time the snapshot was created.
     /// </summary>
     [JsonPropertyName("o")]
@@ -36,7 +51,7 @@ public abstract record ObservationSnapshot<TObs, TValue>
     /// <summary>
     /// Gets a code that discriminates between measurement types and is suitable for use as a hash code.
     /// </summary>
-    public virtual int GetMeasurementHashCode()
+    public override int GetMeasurementHashCode()
     {
         return Observation.GetMeasurementHashCode();
     }
