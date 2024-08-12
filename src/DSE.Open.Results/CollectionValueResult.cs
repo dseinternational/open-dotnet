b3f315114abc01f017c4cd20f1010a3901bf0d37
Frank Buckley <frank@dsegroup.org>
2024-08-12 16:55:12 +0100

    CollectionValueResult: use ReadOnlyValueCollection

--- src/DSE.Open.Results/CollectionValueResult.cs
+++ src/DSE.Open.Results/CollectionValueResult.cs
@@ -2,17 +2,18 @@
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
 using System.Text.Json.Serialization;
+using DSE.Open.Collections.Generic;
 
 namespace DSE.Open.Results;
 
-public record CollectionValueResult<T> : ValueResult<IEnumerable<T>>
+public record CollectionValueResult<T> : ValueResult<ReadOnlyValueCollection<T>>
 {
-    public static new readonly CollectionValueResult<T> Empty = new() { Value = Array.Empty<T>() };
+    public static new readonly CollectionValueResult<T> Empty = new() { Value = [] };
 
     [JsonPropertyName("value")]
-    public override required IEnumerable<T> Value
+    public override required ReadOnlyValueCollection<T> Value
     {
-        get => base.Value ?? Array.Empty<T>();
+        get => base.Value ?? [];
 #pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
         init => base.Value = value;
 #pragma warning restore CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
--- src/DSE.Open.Results/CollectionValueResultBuilder.cs
+++ src/DSE.Open.Results/CollectionValueResultBuilder.cs
@@ -1,20 +1,24 @@
 // Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
+using DSE.Open.Collections.Generic;
+
 namespace DSE.Open.Results;
 
 public abstract class CollectionValueResultBuilder<TResult, TValue>
-    : ValueResultBuilder<TResult, IEnumerable<TValue>>
+    : ValueResultBuilder<TResult, ReadOnlyValueCollection<TValue>>
     where TResult : CollectionValueResult<TValue>
 {
-    private List<TValue> _items = [];
+    private Collection<TValue> _items = [];
 
     public ICollection<TValue> Items => _items;
 
-    public override IEnumerable<TValue>? Value
+#pragma warning disable CA2227 // Collection properties should be read only
+    public override ReadOnlyValueCollection<TValue>? Value
+#pragma warning restore CA2227 // Collection properties should be read only
     {
-        get => _items;
-        set => base.Value = _items = [..value];
+        get => [.._items];
+        set => base.Value = [.. _items = [..value]];
     }
 
     public override void MergeNotificationsAndValue(TResult valueResult)
