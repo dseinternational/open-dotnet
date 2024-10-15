2bacb201d9a33a160e564172bd63bae205f0a332
Ryan Buckley <9486206+rabuckley@users.noreply.github.com>
2024-10-15 15:29:51 +0100

    Take optional JsonSeralizerOptions in AssertJson.Roundtrip

--- src/DSE.Open.Testing.Xunit/AssertJson.cs
+++ src/DSE.Open.Testing.Xunit/AssertJson.cs
@@ -12,13 +12,13 @@ namespace DSE.Open.Testing.Xunit;
 public static class AssertJson
 {
     [RequiresDynamicCode(
-        "JSON serialization and deserialization might require types that cannot be statically analyzed and might need runtime code generation. Use System. Text. Json source generation for native AOT applications.")]
+        "JSON serialization and deserialization might require types that cannot be statically analyzed and might need runtime code generation. Use System.Text.Json source generation for native AOT applications.")]
     [RequiresUnreferencedCode(
         "JSON serialization and deserialization might require types that cannot be statically analyzed. Use the overload that takes a JsonTypeInfo or JsonSerializerContext, or make sure all of the required types are preserved.")]
-    public static void Roundtrip<T>(T value)
+    public static void Roundtrip<T>(T value, JsonSerializerOptions? options = null)
     {
-        var json = JsonSerializer.Serialize(value);
-        var deserialized = JsonSerializer.Deserialize<T>(json);
+        var json = JsonSerializer.Serialize(value, options);
+        var deserialized = JsonSerializer.Deserialize<T>(json, options);
         Assert.Equivalent(value, deserialized);
     }
 
@@ -41,5 +41,4 @@ public static class AssertJson
         var deserialized = JsonSerializer.Deserialize(json, typeof(T), context);
         Assert.Equivalent(value, deserialized);
     }
-
 }
