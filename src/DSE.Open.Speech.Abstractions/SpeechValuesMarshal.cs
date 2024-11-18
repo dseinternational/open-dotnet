5c4ce31d5d8434160809b5480a0a006c8600ceb9
Ryan Buckley <9486206+rabuckley@users.noreply.github.com>
2024-11-18 10:48:56 +0000

    Tidy SpeechSymbolSequence formatting

---
+++ src/DSE.Open.Speech.Abstractions/SpeechValuesMarshal.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+
+namespace DSE.Open.Speech;
+
+internal static class SpeechValuesMarshal
+{
+    public static ReadOnlySpan<char> AsChars(ReadOnlySpan<SpeechSymbol> valueSpan)
+    {
+        return MemoryMarshal.Cast<SpeechSymbol, char>(valueSpan);
+    }
+}
