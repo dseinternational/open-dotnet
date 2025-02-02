5590e561f6afcf82e4335b47646dcdfdb36c6204
Frank Buckley <frank@dsegroup.org>
2025-02-02 16:18:52 +0000

    Update PythonContextConfiguration.cs

--- src/DSE.Open.Interop.Python/PythonContextConfiguration.cs
+++ src/DSE.Open.Interop.Python/PythonContextConfiguration.cs
@@ -1,42 +1,61 @@
 // Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
+using System.Diagnostics;
+
 namespace DSE.Open.Interop.Python;
 
 public sealed class PythonContextConfiguration
 {
+    private static readonly string[] s_environmentVariables =
+    [
+        "PYTHON3_HOME",
+        "Python3_ROOT_DIR",
+        "PYTHON_HOME",
+        "Python_ROOT_DIR",
+        "pythonLocation"
+    ];
+
     public PythonContextConfiguration()
     {
-        string? location = null;
+        var filename = OperatingSystem.IsWindows()
+            ? "python3.13.dll"
+            : OperatingSystem.IsMacOS()
+                ? "libpython3.13.dylib"
+                : "libpython3.13.so";
+
+        var searchLocations = s_environmentVariables
+            .Select(v =>
+            {
+                if (Environment.GetEnvironmentVariable(v) is { Length: > 0 } value)
+                {
+                    return Path.Combine(value, filename);
+                }
+
+                return null;
+            })
+            .Where(s => s is not null)
+            .ToList();
 
         if (OperatingSystem.IsWindows())
         {
-            var localAppData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                "Programs\\Python\\Python313\\python313.dll");
-
-            if (File.Exists(localAppData))
-            {
-                location = localAppData;
-            }
-            else
-            {
-                var programFiles = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
-                    "Python313\\python313.dll");
-
-                if (File.Exists(programFiles))
-                {
-                    location = programFiles;
-                }
-            }
+            searchLocations.Add(
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "Programs\\Python\\Python313\\python313.dll"));
+            searchLocations.Add(
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+                    "Python313\\python313.dll"));
         }
-        else
+        else if (OperatingSystem.IsMacOS())
         {
-            PythonDLL = OperatingSystem.IsLinux()
-                ? "libpython3.13.so"
-                : OperatingSystem.IsMacOS()
-                    ? "/Library/Frameworks/Python.framework/Versions/3.13/bin/python3.13"
-                    : throw new PlatformNotSupportedException();
+            searchLocations.Add("/Library/Frameworks/Python.framework/Versions/3.13/bin/python3.13");
         }
+
+        searchLocations.Add(filename);
+
+        var location = searchLocations.FirstOrDefault(File.Exists);
+
+        Trace.WriteLine($"Using Python 3.13 library found at: {location}");
 
         PythonDLL = location ?? throw new InvalidOperationException("Python 3.13 library not found.");
     }
