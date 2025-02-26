4b1df2fce65f7336e9d2faa97d732acc2032cfa9
Frank Buckley <frank@dsegroup.org>
2025-02-26 17:58:12 +0000

    Vector: add CreateNumeric

--- src/DSE.Open.Numerics/Vector.cs
+++ src/DSE.Open.Numerics/Vector.cs
@@ -10,33 +10,67 @@ namespace DSE.Open.Numerics;
 [JsonDerivedType(typeof(Vector<bool>), "bool")]
 [JsonDerivedType(typeof(Vector<char>), "char")]
 [JsonDerivedType(typeof(Vector<string>), "string")]
-[JsonDerivedType(typeof(NumericVector<byte>), "int8")]
-[JsonDerivedType(typeof(NumericVector<short>), "int16")]
-[JsonDerivedType(typeof(NumericVector<ushort>), "uint16")]
-[JsonDerivedType(typeof(NumericVector<int>), "int32")]
-[JsonDerivedType(typeof(NumericVector<uint>), "uint32")]
-[JsonDerivedType(typeof(NumericVector<long>), "int64")]
-[JsonDerivedType(typeof(NumericVector<uint>), "uint64")]
-[JsonDerivedType(typeof(NumericVector<float>), "float32")]
+[JsonDerivedType(typeof(NumericVector<byte>), "uint8")]
+[JsonDerivedType(typeof(NumericVector<Date64>), "date64")]
 [JsonDerivedType(typeof(NumericVector<double>), "float64")]
-// [JsonDerivedType(typeof(NumericVector<Date64>), "date64")]
+[JsonDerivedType(typeof(NumericVector<float>), "float32")]
+[JsonDerivedType(typeof(NumericVector<int>), "int32")]
+[JsonDerivedType(typeof(NumericVector<Int128>), "int128")]
+[JsonDerivedType(typeof(NumericVector<long>), "int64")]
+[JsonDerivedType(typeof(NumericVector<sbyte>), "int8")]
+[JsonDerivedType(typeof(NumericVector<short>), "int16")]
+[JsonDerivedType(typeof(NumericVector<uint>), "uint32")]
+[JsonDerivedType(typeof(NumericVector<uint>), "uint64")]
+[JsonDerivedType(typeof(NumericVector<UInt128>), "uint128")]
+[JsonDerivedType(typeof(NumericVector<ushort>), "uint16")]
 public abstract class Vector
 {
     public static Vector Create<T>(T[] data)
         where T : notnull
     {
+        EnsureNotNumericType(typeof(T));
         return new Vector<T>(data);
     }
 
     public static Vector Create<T>(Memory<T> data)
         where T : notnull
     {
+        EnsureNotNumericType(typeof(T));
         return new Vector<T>(data);
     }
 
     public static Vector Create<T>(T[] data, int start, int length)
         where T : notnull
     {
+        EnsureNotNumericType(typeof(T));
         return new Vector<T>(data, start, length);
+    }
+
+    public static NumericVector<T> CreateNumeric<T>(T[] data)
+        where T : struct, INumber<T>
+    {
+        return new NumericVector<T>(data);
+    }
+
+    public static NumericVector<T> CreateNumeric<T>(Memory<T> data)
+        where T : struct, INumber<T>
+    {
+        return new NumericVector<T>(data);
+    }
+
+    public static NumericVector<T> CreateNumeric<T>(T[] data, int start, int length)
+        where T : struct, INumber<T>
+    {
+        return new NumericVector<T>(data, start, length);
+    }
+
+    private static void EnsureNotNumericType(Type type)
+    {
+        if (NumberHelper.IsKnownNumberType(type))
+        {
+            ThrowHelper.ThrowInvalidOperationException(
+                $"Cannot create Vector of type {type.Name} as {type.Name} is a numeric type. " +
+                "Call Vector.CreateNumeric<T> instead.");
+        }
     }
 }
--- src/DSE.Open/NumberHelper.cs
+++ src/DSE.Open/NumberHelper.cs
@@ -1,6 +1,9 @@
 // Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
+using System.Collections.Frozen;
+using System.Numerics;
+
 namespace DSE.Open;
 
 public static partial class NumberHelper
@@ -11,5 +14,46 @@ public static partial class NumberHelper
     public static bool IsJsonSafeInteger(ulong value)
     {
         return value <= MaxJsonSafeInteger;
+    }
+
+    private static readonly FrozenSet<Type> s_knownNumberTypes = FrozenSet.Create(
+    [
+        typeof(byte),
+        typeof(Date64),
+        typeof(decimal),
+        typeof(double),
+        typeof(float),
+        typeof(int),
+        typeof(Int128),
+        typeof(long),
+        typeof(sbyte),
+        typeof(short),
+        typeof(uint),
+        typeof(UInt128),
+        typeof(ulong),
+        typeof(ushort),
+    ]);
+
+    /// <summary>
+    /// Determines if the specified type is known to implement <see cref="INumber{TSelf}"/>.
+    /// This is hardcoded to avoid reflection and possible trimming issues.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public static bool IsKnownNumberType(Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type);
+        return s_knownNumberTypes.Contains(type);
+    }
+
+    /// <summary>
+    /// Determines if the specified type is known to implement <see cref="INumber{TSelf}"/>.
+    /// This is hardcoded to avoid reflection and possible trimming issues.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static bool IsKnownNumberType<T>()
+    {
+        return IsKnownNumberType(typeof(T));
     }
 }
