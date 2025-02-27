7b3e604eac4aa67a54fd94c528ea6e45bd5dccbb
Frank Buckley <frank@dsegroup.org>
2025-02-27 18:11:22 +0000

    Update SeriesCollectionExtensions.cs

--- src/DSE.Open.Numerics/SeriesCollectionExtensions.cs
+++ src/DSE.Open.Numerics/SeriesCollectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
 using System.Numerics;
@@ -26,5 +26,12 @@ public static class SeriesCollectionExtensions
     {
         ArgumentNullException.ThrowIfNull(series);
         series.Add(Series.Create(name, Vector.CreateNumeric(vector)));
+    }
+
+    public static void AddNumeric<T>(this ICollection<Series> series, string name, T[] vector, IDictionary<T, Variant> references)
+        where T : struct, INumber<T>
+    {
+        ArgumentNullException.ThrowIfNull(series);
+        series.Add(Series.CreateNumeric(name, Vector.CreateNumeric(vector), references));
     }
 }
