a7d30c30497116bd09b4c0a7397cdee9b7eb62f9
Frank Buckley <frank@dsegroup.org>
2025-03-01 16:03:28 +0000

    Add ValueStringBuilder

--- src/DSE.Open/DiagnosticIds.cs
+++ src/DSE.Open/DiagnosticIds.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
 namespace DSE.Open;
@@ -6,4 +6,5 @@ namespace DSE.Open;
 internal static class DiagnosticIds
 {
     public const string ArrayBuilderRefStructWarning = "DSEOPEN001";
+    public const string ValueStringBuilderRefStructWarning = "DSEOPEN002";
 }
