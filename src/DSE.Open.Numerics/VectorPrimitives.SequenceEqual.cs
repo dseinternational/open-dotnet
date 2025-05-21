615f08d9b0e1e1ead1be4f14baedc73eece18669
Frank Buckley <frank@dsegroup.org>
2025-05-21 15:41:17 +0100

    Update VectorPrimitives.SequenceEqual.cs

--- src/DSE.Open.Numerics/VectorPrimitives.SequenceEqual.cs
+++ src/DSE.Open.Numerics/VectorPrimitives.SequenceEqual.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
+using System;
 using System.Numerics.Tensors;
 
 namespace DSE.Open.Numerics;
@@ -9,37 +10,11 @@ namespace DSE.Open.Numerics;
 
 public static partial class VectorPrimitives
 {
-    public static bool SequenceEqual<T>(this scoped in Span<T> x, scoped in ReadOnlySpan<T> y)
-        where T : IEquatable<T>?
-    {
-        if (x.Length != y.Length)
-        {
-            return false;
-        }
-
-        return Tensor.SequenceEqual(
-            new TensorSpan<T>(x),
-            new ReadOnlyTensorSpan<T>(y));
-    }
-
-    public static bool SequenceEqual<T>(this scoped in ReadOnlySpan<T> x, scoped in ReadOnlySpan<T> y)
-        where T : IEquatable<T>?
-    {
-        if (x.Length != y.Length)
-        {
-            return false;
-        }
-
-        return Tensor.SequenceEqual(
-            new ReadOnlyTensorSpan<T>(x),
-            new ReadOnlyTensorSpan<T>(y));
-    }
-
     public static bool SequenceEqual<T>(this IReadOnlyVector<T> x, scoped in ReadOnlySpan<T> y)
         where T : IEquatable<T>
     {
         ArgumentNullException.ThrowIfNull(x);
-        return SequenceEqual(x.AsSpan(), y);
+        return x.AsSpan().SequenceEqual(y);
     }
 
     public static bool SequenceEqual<T>(this IReadOnlyVector<T> x, IReadOnlyVector<T> y)
@@ -47,6 +22,6 @@ public static partial class VectorPrimitives
     {
         ArgumentNullException.ThrowIfNull(x);
         ArgumentNullException.ThrowIfNull(y);
-        return SequenceEqual(x.AsSpan(), y.AsSpan());
+        return x.AsSpan().SequenceEqual(y.AsSpan());
     }
 }
