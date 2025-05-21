45dedc7d35df7332e15ea87cdf9d869fc9ce1ea2
Frank Buckley <frank@dsegroup.org>
2025-05-21 18:57:52 +0100

    Add SeriesPrimitives.Subtract

--- src/DSE.Open.Numerics/SeriesPrimitives.Subtract.cs
+++ src/DSE.Open.Numerics/SeriesPrimitives.Subtract.cs
@@ -2,65 +2,64 @@
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
 using System.Numerics;
-using System.Numerics.Tensors;
 
 namespace DSE.Open.Numerics;
 
-public static partial class VectorPrimitives
+public static partial class SeriesPrimitivesSubtract
 {
-    public static void Subtract<T>(this IReadOnlyVector<T> x, ReadOnlySpan<T> y, Span<T> destination)
+    public static void Subtract<T>(this IReadOnlySeries<T> x, ReadOnlySpan<T> y, Span<T> destination)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(x);
         NumericsException.ThrowIfNot(x.Length == y.Length && y.Length == destination.Length);
-        TensorPrimitives.Subtract(x.AsSpan(), y, destination);
+        VectorPrimitives.Subtract(x.Vector, y, destination);
     }
 
-    public static void Subtract<T>(this IReadOnlyVector<T> x, IReadOnlyVector<T> y, Span<T> destination)
+    public static void Subtract<T>(this IReadOnlySeries<T> x, IReadOnlySeries<T> y, Span<T> destination)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(y);
-        Subtract(x, y.AsSpan(), destination);
-        Subtract(x, y.AsSpan(), destination);
+        Subtract(x, y.Vector.AsSpan(), destination);
+        Subtract(x, y.Vector.AsSpan(), destination);
     }
 
-    public static void Subtract<T>(this IReadOnlyVector<T> x, IReadOnlyVector<T> y, IVector<T> destination)
+    public static void Subtract<T>(this IReadOnlySeries<T> x, IReadOnlySeries<T> y, ISeries<T> destination)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(destination);
         Subtract(x, y, destination.AsSpan());
     }
 
-    public static void Subtract<T>(this IReadOnlyVector<T> x, T y, Span<T> destination)
+    public static void Subtract<T>(this IReadOnlySeries<T> x, T y, Span<T> destination)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(x);
         NumericsException.ThrowIfNot(x.Length == destination.Length);
-        TensorPrimitives.Subtract(x.AsSpan(), y, destination);
+        VectorPrimitives.Subtract(x.Vector, y, destination);
     }
 
-    public static void Subtract<T>(this IReadOnlyVector<T> x, T y, IVector<T> destination)
+    public static void Subtract<T>(this IReadOnlySeries<T> x, T y, ISeries<T> destination)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(destination);
         Subtract(x, y, destination.AsSpan());
     }
 
-    public static void SubtractInPlace<T>(this IVector<T> x, ReadOnlySpan<T> y)
+    public static void SubtractInPlace<T>(this ISeries<T> x, ReadOnlySpan<T> y)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(x);
         Subtract(x, y, x.AsSpan());
     }
 
-    public static void SubtractInPlace<T>(this IVector<T> x, IReadOnlyVector<T> y)
+    public static void SubtractInPlace<T>(this ISeries<T> x, IReadOnlySeries<T> y)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(y);
-        SubtractInPlace(x, y.AsSpan());
+        SubtractInPlace(x, y.Vector.AsSpan());
     }
 
-    public static void SubtractInPlace<T>(this IVector<T> x, T y)
+    public static void SubtractInPlace<T>(this ISeries<T> x, T y)
         where T : struct, INumber<T>
     {
         ArgumentNullException.ThrowIfNull(x);
