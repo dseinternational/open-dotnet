f4ff89a7f7e1e52ec56b70005ad0683dbe5ae95a
Frank Buckley <5655810+frankbuckley@users.noreply.github.com>
2025-07-08 09:33:44 +0100

    Convert to .slnx

--- eng/scripts/update-dependencies.ps1
+++ eng/scripts/update-dependencies.ps1
@@ -19,7 +19,7 @@ function Update-Dependencies() {
     Write-Host "--------------------------------------------------------------------------------" -ForegroundColor Cyan
     Write-Host
 
-    &dotnet-outdated -pre auto --upgrade --no-restore ./DSE.Open.sln
+    &dotnet-outdated -pre auto --upgrade --no-restore ./DSE.Open.slnx
 }
 
 try {
