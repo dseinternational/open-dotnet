56e07cd58830614dd43ecb04e15a0d091da2592e
Frank Buckley <frank@frankbuckley.com>
2025-10-17 09:39:39 +0100

    Update dependencies

--- eng/scripts/init_python.ps1
+++ eng/scripts/init_python.ps1
@@ -41,8 +41,8 @@ function TestEnvironment-Init() {
 
   if ($IsWindows) {
     if ($env:USE_CUDA -eq "True") {
-      Write-Host "Using CUDA 12.9"
-      python -m pip install --upgrade torch --index-url https://download.pytorch.org/whl/cu129
+      Write-Host "Using CUDA  13.0"
+      python -m pip install --upgrade torch --index-url https://download.pytorch.org/whl/cu130
     }
     else {
       Write-Host "Using CPU only"
@@ -51,8 +51,8 @@ function TestEnvironment-Init() {
   }
   elseif ($IsLinux) {
     if ($env:USE_CUDA -eq "True") {
-      Write-Host "Using CUDA 12.9"
-      python -m pip install --upgrade torch --index-url https://download.pytorch.org/whl/cu129
+      Write-Host "Using CUDA  13.0"
+      python -m pip install --upgrade torch --index-url https://download.pytorch.org/whl/cu130
     }
     else {
       Write-Host "Using CPU only"
